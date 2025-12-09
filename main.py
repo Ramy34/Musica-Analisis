bc3f6263d73d686a0bdad6d8d6e684b1fd90ff5b
Ramsés Castro <ramses7814@gmail.com>
2025-12-08 21:33:58 -0600

    Se agregan los catalogos de artistas y la desnormalización de las canciones

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import multiprocessing
 from config.config_loader import Config
-from code.python import extraccion_metdatos, xml_to_csv, carga_snowflake
+from code.python import extraccion_metdatos, xml_to_csv, carga_snowflake, artistas, canciones_desnormalizadas
 
 def main():
     config = Config()
@@ -8,6 +8,8 @@ def main():
     if config.general["procesar_todo"]:
         extraccion_metdatos.main(config.paths["music_folder"], config.paths["csv_metadata_output"], config.general["threads"], config.general["escala"])
     xml_to_csv.main(config.paths["input_folder"], config.paths["output_folder"])
+    artistas.main(config.paths["csv_metadata_output"], config.paths["csv_artistas_output"])
+    canciones_desnormalizadas.main(config.paths["csv_metadata_output"], config.paths["csv_artistas_output"], config.paths["csv_canciones_output"])
     carga_snowflake.main(config.snowflake_credentials["account"], config.snowflake_credentials["user"], config.snowflake_credentials["password"], config.snowflake["warehouse"], config.snowflake["database"], config.paths["output_folder"])
 
 if __name__ == "__main__":
