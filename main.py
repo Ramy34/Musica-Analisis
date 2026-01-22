b6ae421fbc5b92dec4c6d6dd756f6102bbbf2842
Ramsés Castro <ramses7814@gmail.com>
2026-01-21 23:06:42 -0600

    Para la extracción de metadata se quita la medición por escala y se agrega la función de visualizar por cada 10% del total

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ def main():
     config = Config()
 
     if config.general["procesar_todo"]:
-        extraccion_metdatos.main(config.paths["music_folder"], config.paths["csv_metadata_output"], config.general["threads"], config.general["escala"])
-    xml_to_csv.main(config.paths["input_folder"], config.paths["output_folder"])
+        extraccion_metdatos.main(config.paths["music_folder"], config.paths["csv_metadata_output"], config.general["threads"])
+    '''xml_to_csv.main(config.paths["input_folder"], config.paths["output_folder"])
     unir_csv.main(config.paths["csv_preraw"], config.paths["csv_raw"])
     artistas.main(config.paths["csv_metadata_output"], config.paths["csv_artistas_output"])
     canciones_desnormalizadas.main(config.paths["csv_metadata_output"], config.paths["csv_artistas_output"], config.paths["csv_canciones_output"])
     carga_snowflake.main(config.snowflake_credentials["account"], config.snowflake_credentials["user"], config.snowflake_credentials["password"], config.snowflake["warehouse"], config.snowflake["database"], config.paths["output_folder"])
-
+'''
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     main()
